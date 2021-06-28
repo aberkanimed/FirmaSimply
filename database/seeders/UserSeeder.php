4dd3cdfd9563bbc0a8134a8cad64499f25784dea
Mohamed Aberkani <aberkanimed@gmail.com>
2021-06-28 08:02:48 +0200

    seed users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,33 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create(['email' => 'john@doe.com']);
+        $users = [
+                    ['joaquin@firmasimply.com','password'],
+                    ['rosario@firmasimply.com','password'],
+                    ['alicia@firmasimply.com','password'],
+                    ['pablo@firmasimply.com','password'],
+                    ['tadeo@firmasimply.com','password'],
+                    ['derian@firmasimply.com','password'],
+                    ['javier@firmasimply.com','password'],
+                    ['manuel.igor@firmasimply.com','password'],
+                    ['moisis@firmasimply.com','password'],
+                    ['jose.manuel@firmasimply.com','password'],
+                    ['antonio@firmasimply.com','password'],
+                    ['jimena@firmasimply.com','password'],
+                    ['roberto.carlos@firmasimply.com','password'],
+                    ['daniel@firmasimply.com','password'],
+                    ['manuel.francisco@firmasimply.com','password'],
+                    ['oscar@firmasimply.com','password'],
+                    ['jose.enrique@firmasimply.com','password'],
+                    ['maria@firmasimply.com','password'],
+                    ['aurora@firmasimply.com','password'],
+                    ['guillermo@firmasimply.com','password'],
+                    ['ruben@firmasimply.com','password'],
+                    ['matias@firmasimply.com','password'],
+                    ['mohammad@firmasimply.com','password']
+                ];
+        foreach ($users as $user) {
+            User::factory()->create(['email' => $user[0]]);
+        }
     }
 }
